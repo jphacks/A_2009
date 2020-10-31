0950d46839f266154fae6da99ab886141ee9d74a
山下陽央 <orauutandayo@gmail.com>
2020-10-31 11:33:20 +0900

    QR

--- jphacks2020_flutter/lib/main.dart
+++ jphacks2020_flutter/lib/main.dart
@@ -1,47 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'QR scaner And QR Maker',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'QR scaner And QR Maker'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,66 +27,69 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // 読み取り結果を格納するString型の変数readData
+  String readData = "";
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  /// QR及びバーコードスキャンを行うメソッドscan()
+  /// Future:非同期処理を実現する
+  Future scan() async {
+    try {
+      // String型のcodeにBarcodeScanner.scan()の結果を代入
+      // await：非同期対応の要素のキーワード
+      ScanResult code = await BarcodeScanner.scan();
+      // readDataに読み取ったデータを格納する
+      setState(() => this.readData = code as String);
+    }
+    // 例外処理：プラグインが何らかのエラーを出したとき
+    on PlatformException catch (e) {
+      // エラーコードがBarcodeScanner.CameraAccessDeniedであるときは、
+      // このアプリにカメラ機能のパーミッションを許可していない状態であることを示す
+      if (e.code == BarcodeScanner.cameraAccessDenied) {
+        setState(() {
+          // readDataにエラー内容を代入
+          this.readData = 'カメラのパーミッションが有効になっていません。';
+        });
+      }
+      // その他の場合は不明のエラー
+      else {
+        setState(() => this.readData = '原因不明のエラー: $e');
+      }
+    }
+    // 意図しない入力、操作を受けたとき
+    on FormatException{
+      setState(() => this.readData = '読み取れませんでした (スキャンを開始する前に戻るボタンを使用しました)');
+    } catch (e) {
+      setState(() => this.readData = '不明なエラー: $e');
+    }
   }
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'QRボタンを押すと読み取りを開始します',
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              // 読み取り結果readDataを表示
+              '$readData',
+              style: Theme.of(context).textTheme.display1,
             ),
           ],
         ),
       ),
+      // ボタンを用意する
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        // onPressed:ボタンを押すことでscan()という関数が実行される
+        onPressed: scan,
+        tooltip: 'QR SCAN',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
