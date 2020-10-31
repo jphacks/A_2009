91c10048effbd1b3c6382a58a2a383be1f0f9543
山下陽央 <orauutandayo@gmail.com>
2020-10-31 13:28:24 +0900

    Camera view

--- jphacks2020_flutter/lib/main.dart
+++ jphacks2020_flutter/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:barcode_scan/barcode_scan.dart';
-import 'package:flutter/services.dart';
+import 'package:jphacks2020/qr_read_view.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,86 +11,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'QR scaner And QR Maker'),
+      home: MainView(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // 読み取り結果を格納するString型の変数readData
-  String readData = "";
-
-  /// QR及びバーコードスキャンを行うメソッドscan()
-  /// Future:非同期処理を実現する
-  Future scan() async {
-    try {
-      // String型のcodeにBarcodeScanner.scan()の結果を代入
-      // await：非同期対応の要素のキーワード
-      ScanResult code = await BarcodeScanner.scan();
-      // readDataに読み取ったデータを格納する
-      setState(() => this.readData = code as String);
-    }
-    // 例外処理：プラグインが何らかのエラーを出したとき
-    on PlatformException catch (e) {
-      // エラーコードがBarcodeScanner.CameraAccessDeniedであるときは、
-      // このアプリにカメラ機能のパーミッションを許可していない状態であることを示す
-      if (e.code == BarcodeScanner.cameraAccessDenied) {
-        setState(() {
-          // readDataにエラー内容を代入
-          this.readData = 'カメラのパーミッションが有効になっていません。';
-        });
-      }
-      // その他の場合は不明のエラー
-      else {
-        setState(() => this.readData = '原因不明のエラー: $e');
-      }
-    }
-    // 意図しない入力、操作を受けたとき
-    on FormatException{
-      setState(() => this.readData = '読み取れませんでした (スキャンを開始する前に戻るボタンを使用しました)');
-    } catch (e) {
-      setState(() => this.readData = '不明なエラー: $e');
-    }
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'QRボタンを押すと読み取りを開始します',
-            ),
-            Text(
-              // 読み取り結果readDataを表示
-              '$readData',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      // ボタンを用意する
-      floatingActionButton: FloatingActionButton(
-        // onPressed:ボタンを押すことでscan()という関数が実行される
-        onPressed: scan,
-        tooltip: 'QR SCAN',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
---
+++ jphacks2020_flutter/lib/qr_read_view.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
+
+class MainView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                onPrimary: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text('カメラで追加'),
+              onPressed: () {
+                _moveToQrReadView(context);
+              },
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                onPrimary: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text('履歴'),
+              onPressed: () {
+                // _getImageFromDevice(ImageSource.gallery);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future _moveToQrReadView(BuildContext context) => Navigator.push(context,
+      MaterialPageRoute<void>(builder: (context) => const QrReadView()));
+}
+
+class QrReadView extends StatefulWidget {
+  const QrReadView({Key key}) : super(key: key);
+
+  @override
+  _QrReadViewState createState() => _QrReadViewState();
+}
+
+class _QrReadViewState extends State<QrReadView> {
+  // 読み取り結果を格納するString型の変数readData
+  String readData = '';
+
+  /// QR及びバーコードスキャンを行うメソッドscan()
+  /// Future:非同期処理を実現する
+  Future scan() async {
+    try {
+      // String型のcodeにBarcodeScanner.scan()の結果を代入
+      // await：非同期対応の要素のキーワード
+      final code = await BarcodeScanner.scan();
+
+      // readDataに読み取ったデータを格納する
+      setState(() => readData = code.rawContent);
+    }
+    // 例外処理：プラグインが何らかのエラーを出したとき
+    on PlatformException catch (e) {
+      // エラーコードがBarcodeScanner.CameraAccessDeniedであるときは、
+      // このアプリにカメラ機能のパーミッションを許可していない状態であることを示す
+      if (e.code == BarcodeScanner.cameraAccessDenied) {
+        setState(() {
+          // readDataにエラー内容を代入
+          readData = 'カメラのパーミッションが有効になっていません。';
+        });
+      }
+      // その他の場合は不明のエラー
+      else {
+        setState(() => readData = '原因不明のエラー: $e');
+      }
+    }
+    // 意図しない入力、操作を受けたとき
+    on FormatException {
+      setState(() => readData = '読み取れませんでした (スキャンを開始する前に戻るボタンを使用しました)');
+    } on Exception catch (e) {
+      setState(() => readData = '不明なエラー: $e');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    scan();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              '読み込めませんでした',
+            ),
+            Text(
+              // 読み取り結果readDataを表示
+              '$readData',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            ElevatedButton(
+              child: const Text('もう一度'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                onPrimary: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: scan,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
