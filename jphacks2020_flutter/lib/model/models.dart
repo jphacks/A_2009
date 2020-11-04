2c0fbf91ecb887048d07ed31a3910dd44c492df5
山下陽央 <orauutandayo@gmail.com>
2020-11-05 04:51:51 +0900

    json test

--- jphacks2020_flutter/lib/model/models.dart
+++ jphacks2020_flutter/lib/model/models.dart
@@ -1,3 +1,6 @@
+// import 'dart:convert';
+
+// import 'package:flutter/services.dart' show rootBundle;
 import 'package:uuid/uuid.dart';
 
 class Comment {
@@ -24,3 +27,19 @@ class Presentation {
   String date;
   String author;
 }
+
+
+// List<dynamic> _data;
+//
+// Future<String> _loadAVaultAsset() async {
+//   return rootBundle.loadString('json/api_name.json');
+// }
+//
+// Future getLocalTestJSONData() async {
+//   final jsonString = await _loadAVaultAsset();
+//   setState(() {
+//     final dynamic jsonResponse = json.decode(jsonString);
+//     print('### getLocalTestJSONData:$jsonResponse');
+//     _data = jsonResponse['categorydata'] as List<dynamic>;
+//   });
+// }
